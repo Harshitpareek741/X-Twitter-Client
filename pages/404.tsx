bb4ef76d3c60be09d4c3cadd3e59662720e9a715
harshit <harshitpareek893@gmail.com>
2024-08-22 09:14:08 +0530

    d

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,5 @@
 // pages/404.js
+import Link from 'next/link';
 import React from 'react';
 
 const Custom404 = () => {
@@ -6,7 +7,7 @@ const Custom404 = () => {
     <div style={{ textAlign: 'center', padding: '50px' }}>
       <h1 className='text-3xl font-semibold'>Sorry</h1>
       <p className='text-2xl'>This Page is Still in Progres or Not available</p>
-      <a href="/" className='underline'>Click Here</a>
+      <Link href="/" className='underline'>Click Here</Link>
       <h2></h2>
     </div>
   );
