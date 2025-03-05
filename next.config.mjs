27266cae9ebc94653c9e3391685697f13165167d
Harshitpareek741 <harshitpareek132@gmail.com>
2025-03-05 15:05:16 +0530

    update

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
   eslint : {
     ignoreDuringBuilds: true , 
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images : {
     domains: ['images.macrumors.com','lh3.googleusercontent.com','pbs.twimg.com',
       "twitter-devv.s3.ap-south-1.amazonaws.com"
